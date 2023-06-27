f44f72a8e42f4ad9de71c2994d09e912d88f5dcf
Andrea Chavez <andychavez02@gmail.com>
2023-06-27 02:04:19 -0300

    script now working with pyspark

--- README.md
+++ README.md
@@ -4,12 +4,11 @@ Proyect for Data Engineering course, retrieve of Coronal Mass Ejection data from
 ## Steps
 1. Create an .env file with the following structure:
 ```
-AWS_REDSHIFT_USER=your-user
-AWS_REDSHIFT_PASSWORD=your-password
-AWS_REDSHIFT_HOST=data-engineer-cluster.cyhh5bfevlmn.us-east-1.redshift.amazonaws.com
-AWS_REDSHIFT_PORT=5439
-AWS_REDSHIFT_DBNAME=data-engineer-database
-AWS_REDSHIFT_SCHEMA=your-schema
+REDSHIFT_USER=your-user
+REDSHIFT_PASSWORD=your-password
+REDSHIFT_HOST=data-engineer-cluster.cyhh5bfevlmn.us-east-1.redshift.amazonaws.com
+REDSHIFT_PORT=5439
+REDSHIFT_DB=data-engineer-database
 ```
-2. Run "script.py"
+2. Run "script.py" inside the Docker container provided by the teachers
 3. To check that the data is also in the db, you can execute "select * from amchavezaltamirano_coderhouse.coronal_mass_ejection;"
--- script.py
+++ script.py
@@ -1,35 +1,25 @@
-#https://api.nasa.gov/DONKI/CMEAnalysis?mostAccurateOnly=true&speed=500&halfAngle=30&catalog=ALL&api_key=DEMO_KEY
-#API from https://api.nasa.gov/
-
+#!pip install psycopg2-binary
 import requests
 import json
-import pandas as pd
-from pandas.core.reshape.merge import string
 from datetime import datetime
-from tabulate import tabulate
-import os
-from dotenv import load_dotenv
+from os import environ as env
+from pyspark.sql import SparkSession
+import pyspark.pandas as ps
+from pyspark.sql.functions import when, lit, col
 import psycopg2
-from sqlalchemy import create_engine
+from pyspark.sql.functions import col, udf, substring
+from pyspark.sql.types import StringType
 
-#environment variables
-load_dotenv()
-
-AWS_REDSHIFT_USER = os.getenv('AWS_REDSHIFT_USER')
-AWS_REDSHIFT_PASSWORD = os.getenv('AWS_REDSHIFT_PASSWORD')
-AWS_REDSHIFT_HOST = os.getenv('AWS_REDSHIFT_HOST')
-AWS_REDSHIFT_PORT = os.getenv('AWS_REDSHIFT_PORT')
-AWS_REDSHIFT_DBNAME = os.getenv('AWS_REDSHIFT_DBNAME')
-AWS_REDSHIFT_SCHEMA = os.getenv('AWS_REDSHIFT_SCHEMA')
-AWS_REDSHIFT_URL = "postgresql+psycopg2://"+AWS_REDSHIFT_USER+":"+AWS_REDSHIFT_PASSWORD+"@"+AWS_REDSHIFT_HOST+":"+AWS_REDSHIFT_PORT+"/"+AWS_REDSHIFT_DBNAME
-DRIVER_PATH = os.getenv('DRIVER_PATH')
-
-#some auxiliary functions
-def date_convert(date_to_convert):
-     return datetime.strptime(date_to_convert, '%Y-%m-%dT%H:%MZ').strftime('%Y-%m-%d')
-
-def time_convert(date_to_convert):
-     return datetime.strptime(date_to_convert, '%Y-%m-%dT%H:%MZ').strftime('%H:%M:%S')
+AWS_REDSHIFT_USER = env["REDSHIFT_USER"]
+AWS_REDSHIFT_PASSWORD = env["REDSHIFT_PASSWORD"]
+AWS_REDSHIFT_HOST = env['REDSHIFT_HOST']
+AWS_REDSHIFT_PORT = env['REDSHIFT_PORT']
+AWS_REDSHIFT_DBNAME = env['REDSHIFT_DB']
+AWS_REDSHIFT_SCHEMA = 'amchavezaltamirano_coderhouse'
+DRIVER_PATH = "/home/coder/working_dir/spark_drivers/postgresql-42.5.2.jar"
+REDSHIFT_URL = f"jdbc:postgresql://{AWS_REDSHIFT_HOST}:{AWS_REDSHIFT_PORT}/{AWS_REDSHIFT_DBNAME}?user={AWS_REDSHIFT_USER}&password={AWS_REDSHIFT_PASSWORD}"
+env['PYSPARK_SUBMIT_ARGS'] = f'--driver-class-path {DRIVER_PATH} --jars {DRIVER_PATH} pyspark-shell'
+env['SPARK_CLASSPATH'] = DRIVER_PATH
 
 #get todays date
 today = datetime.today().strftime('%Y-%m-%d')
@@ -44,6 +34,14 @@ if result.status_code == 200:
 else:
     print(f"Error retrieving data, status code: {response.status_code}")
 
+# Create SparkSession 
+spark = SparkSession.builder \
+        .master("local[1]") \
+        .appName("Conexion entre Pyspark y Redshift") \
+        .config("spark.jars", DRIVER_PATH) \
+        .config("spark.executor.extraClassPath", DRIVER_PATH) \
+        .getOrCreate()
+
 # Connect to Redshift using psycopg2
 conn = psycopg2.connect(
     host=AWS_REDSHIFT_HOST,
@@ -52,7 +50,7 @@ conn = psycopg2.connect(
     user=AWS_REDSHIFT_USER,
     password=AWS_REDSHIFT_PASSWORD
 )
-engine= create_engine(AWS_REDSHIFT_URL)
+
 cursor = conn.cursor()
 cursor.execute(f"""
 create table if not exists {AWS_REDSHIFT_SCHEMA}.coronal_mass_ejection(
@@ -64,8 +62,8 @@ speed float not null,
 type_event text not null,
 isMostAccurate boolean not null,
 associatedCMEID varchar(28),
-note varchar(600),
-catalog_event text,
+note varchar(256),
+catalog_event varchar(256),
 link text,
 date_event date not null,
 time_event time not null,
@@ -78,38 +76,59 @@ conn.commit()
 cursor.close()
 print("Table created!")
 
-#load data into DataFrame
-dataFrame = pd.DataFrame(data)
+#some auxiliary functions
+def date_convert(date_to_convert):
+     return datetime.strptime(date_to_convert, '%Y-%m-%dT%H:%MZ').strftime('%Y-%m-%d')
 
-#rename some columns and some format for database upload
-dataFrame.rename(columns = {'time21_5':'datetime_event', 'type':'type_event', 'catalog':'catalog_event'}, inplace = True)
+def time_convert(date_to_convert):
+     return datetime.strptime(date_to_convert, '%Y-%m-%dT%H:%MZ').strftime('%H:%M:%S')
 
-dataFrame['date_event'] = dataFrame['datetime_event'].apply(date_convert)
-dataFrame['time_event'] = dataFrame['datetime_event'].apply(time_convert)
+df = spark.createDataFrame(data)
+df.printSchema()
+df.show()
+#Some transformations
+df = df.dropna()
+df = df.dropDuplicates(['time21_5'])
+df = df.withColumnRenamed('time21_5','datetime_event')
+df = df.withColumnRenamed('type','type_event')
+df = df.withColumnRenamed('catalog','catalog_event')
+dateUDF = udf(lambda x:date_convert(x),StringType()) 
+timeUDF = udf(lambda x:time_convert(x),StringType())
+df = df.withColumn('date_event', dateUDF(df.datetime_event))
+df = df.withColumn('time_event', timeUDF(df.datetime_event))
+df = df.withColumn('datetime_event', col('datetime_event').cast('string'))
+df = df.withColumn('datetime_event', col('datetime_event').cast('string'))
+df = df.withColumn('datetime_event', col('datetime_event').cast('string'))
+df = df.withColumn('note', substring(col("note"),0,255))
+df = df.withColumn('type_event', col('type_event').cast('string'))
+df = df.withColumn('catalog_event', col('catalog_event').cast('string'))
+df = df.withColumn('link', col('link').cast('string'))
+df = df.withColumn('associatedCMEID', col('associatedCMEID').cast('string'))
+df = df.withColumn('latitude', col('latitude').cast('int'))
+df = df.withColumn('longitude', col('longitude').cast('int'))
+df = df.withColumn('halfAngle', col('halfAngle').cast('int'))
+df = df.withColumn('speed', col('speed').cast('int'))
 
-dataFrame = dataFrame.astype({'latitude':'int','longitude':'int','halfAngle':'int','speed':'int'})
-dataFrame = dataFrame.astype({'datetime_event':'string','type_event':'string','catalog_event':'string','associatedCMEID':'string','link':'string'})
-dataFrame = dataFrame.drop_duplicates()
+df.show()
+df.write \
+    .format("jdbc") \
+    .option("url", REDSHIFT_URL) \
+    .option("dbtable", f"amchavezaltamirano_coderhouse.coronal_mass_ejection") \
+    .option("user", AWS_REDSHIFT_USER) \
+    .option("password", AWS_REDSHIFT_PASSWORD) \
+    .option("driver", "org.postgresql.Driver") \
+    .mode("overwrite") \
+    .save()
 
-print('\nData frame information:')
-print(tabulate(dataFrame, headers='keys', tablefmt='psql')) 
-
-#upload to redshift
-import redshift_connector
-
-redshiftConn = redshift_connector.connect(
-   host=AWS_REDSHIFT_HOST,
-   database=AWS_REDSHIFT_DBNAME,
-   port=5439,
-   user=AWS_REDSHIFT_USER,
-   password=AWS_REDSHIFT_PASSWORD
-)
-
-redshiftConn.autocommit = True
-cursor = redshiftConn.cursor()
-cursor.write_dataframe(dataFrame, 'amchavezaltamirano_coderhouse.coronal_mass_ejection')
 print("Data inserted!")
-#check that the information is in the db
-cursor.execute("select * from amchavezaltamirano_coderhouse.coronal_mass_ejection;")
-result = cursor.fetchall()
-print(tabulate(result, headers='keys', tablefmt='psql'))
+print("Retrieving data from database...")
+query = f"select * from amchavezaltamirano_coderhouse.coronal_mass_ejection"
+data = spark.read \
+    .format("jdbc") \
+    .option("url", REDSHIFT_URL) \
+    .option("dbtable", f"({query}) as tmp_table") \
+    .option("user", AWS_REDSHIFT_USER) \
+    .option("password", AWS_REDSHIFT_PASSWORD) \
+    .option("driver", "org.postgresql.Driver") \
+    .load()
+data.show()
