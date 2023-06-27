60775b603f95cc0fce5975c5a463c5f31d6caa20
Andrea Chavez <andychavez02@gmail.com>
2023-06-27 00:24:42 -0300

    improve script, add gitignore

--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 Proyect for Data Engineering course
 
 ## Steps
-1. Run "tableCreation.sql" after connecting to Redshift database
-2. In "script.py" replace "dummyUsername" and "dummyPassword" with your username and password
-3. Run "script.py"
-4. To check that the data is also in the db, you can execute "select * from amchavezaltamirano_coderhouse.coronal_mass_ejection;"
+1. Create an .env file with the following structure:
+```
+AWS_REDSHIFT_USER=your-user
+AWS_REDSHIFT_PASSWORD=your-password
+AWS_REDSHIFT_HOST=data-engineer-cluster.cyhh5bfevlmn.us-east-1.redshift.amazonaws.com
+AWS_REDSHIFT_PORT=5439
+AWS_REDSHIFT_DBNAME=data-engineer-database
+AWS_REDSHIFT_SCHEMA=your-schema
+```
+2. Run "script.py"
+3. To check that the data is also in the db, you can execute "select * from amchavezaltamirano_coderhouse.coronal_mass_ejection;"
--- script.py
+++ script.py
@@ -7,7 +7,24 @@ import pandas as pd
 from pandas.core.reshape.merge import string
 from datetime import datetime
 from tabulate import tabulate
+import os
+from dotenv import load_dotenv
+import psycopg2
+from sqlalchemy import create_engine
 
+#environment variables
+load_dotenv()
+
+AWS_REDSHIFT_USER = os.getenv('AWS_REDSHIFT_USER')
+AWS_REDSHIFT_PASSWORD = os.getenv('AWS_REDSHIFT_PASSWORD')
+AWS_REDSHIFT_HOST = os.getenv('AWS_REDSHIFT_HOST')
+AWS_REDSHIFT_PORT = os.getenv('AWS_REDSHIFT_PORT')
+AWS_REDSHIFT_DBNAME = os.getenv('AWS_REDSHIFT_DBNAME')
+AWS_REDSHIFT_SCHEMA = os.getenv('AWS_REDSHIFT_SCHEMA')
+AWS_REDSHIFT_URL = "postgresql+psycopg2://"+AWS_REDSHIFT_USER+":"+AWS_REDSHIFT_PASSWORD+"@"+AWS_REDSHIFT_HOST+":"+AWS_REDSHIFT_PORT+"/"+AWS_REDSHIFT_DBNAME
+DRIVER_PATH = os.getenv('DRIVER_PATH')
+
+#some auxiliary functions
 def date_convert(date_to_convert):
      return datetime.strptime(date_to_convert, '%Y-%m-%dT%H:%MZ').strftime('%Y-%m-%d')
 
@@ -17,16 +34,49 @@ def time_convert(date_to_convert):
 #get todays date
 today = datetime.today().strftime('%Y-%m-%d')
 
-#get data from api
-response = requests.get('https://api.nasa.gov/DONKI/CMEAnalysis?startDate=2019-01-01&endDate='+today+'&mostAccurateOnly=true&speed=500&halfAngle=30&catalog=ALL&api_key=DEMO_KEY')
+result = requests.get('https://api.nasa.gov/DONKI/CMEAnalysis?startDate=2023-06-10&endDate='+today+'&mostAccurateOnly=true&speed=500&halfAngle=30&catalog=ALL&api_key=DEMO_KEY')
 
 #handle api response
-if response.status_code == 200:
-    data = json.loads(response.text)
+if result.status_code == 200:
+    data = json.loads(result.text)
     print('Data retrieved from API:')
     print(data)
 else:
     print(f"Error retrieving data, status code: {response.status_code}")
+
+# Connect to Redshift using psycopg2
+conn = psycopg2.connect(
+    host=AWS_REDSHIFT_HOST,
+    port=AWS_REDSHIFT_PORT,
+    dbname=AWS_REDSHIFT_DBNAME,
+    user=AWS_REDSHIFT_USER,
+    password=AWS_REDSHIFT_PASSWORD
+)
+engine= create_engine(AWS_REDSHIFT_URL)
+cursor = conn.cursor()
+cursor.execute(f"""
+create table if not exists {AWS_REDSHIFT_SCHEMA}.coronal_mass_ejection(
+datetime_event text not null,
+latitude float not null,
+longitude float not null,
+halfAngle float not null,
+speed float not null,
+type_event text not null,
+isMostAccurate boolean not null,
+associatedCMEID varchar(28),
+note varchar(600),
+catalog_event text,
+link text,
+date_event date not null,
+time_event time not null,
+id int identity(1,1),
+primary key(id))
+distkey(date_event)
+compound sortkey(id, date_event,time_event);
+""")
+conn.commit()
+cursor.close()
+print("Table created!")
 
 #load data into DataFrame
 dataFrame = pd.DataFrame(data)
@@ -39,24 +89,26 @@ dataFrame['time_event'] = dataFrame['datetime_event'].apply(time_convert)
 
 dataFrame = dataFrame.astype({'latitude':'int','longitude':'int','halfAngle':'int','speed':'int'})
 dataFrame = dataFrame.astype({'datetime_event':'string','type_event':'string','catalog_event':'string','associatedCMEID':'string','link':'string'})
+dataFrame = dataFrame.drop_duplicates()
 
 print('\nData frame information:')
 print(tabulate(dataFrame, headers='keys', tablefmt='psql')) 
 
-#upload to redshift, remember to execute "TableCreation.sql" first
+#upload to redshift
 import redshift_connector
 
-conn = redshift_connector.connect(
-   host='data-engineer-cluster.cyhh5bfevlmn.us-east-1.redshift.amazonaws.com',
-   database='data-engineer-database',
+redshiftConn = redshift_connector.connect(
+   host=AWS_REDSHIFT_HOST,
+   database=AWS_REDSHIFT_DBNAME,
    port=5439,
-   user='dummyUsername',
-   password='dummyPassword'
+   user=AWS_REDSHIFT_USER,
+   password=AWS_REDSHIFT_PASSWORD
 )
 
-conn.autocommit = True
-cursor = conn.cursor()
+redshiftConn.autocommit = True
+cursor = redshiftConn.cursor()
 cursor.write_dataframe(dataFrame, 'amchavezaltamirano_coderhouse.coronal_mass_ejection')
+print("Data inserted!")
 #check that the information is in the db
 cursor.execute("select * from amchavezaltamirano_coderhouse.coronal_mass_ejection;")
 result = cursor.fetchall()
